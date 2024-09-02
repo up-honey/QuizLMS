b81ec3d9f586c2e2e46adc2191f8a577027830b2
shshsho <89124179+shshsho@users.noreply.github.com>
2024-09-02 11:41:35 +0900

    기본 폴더 생성
    9.2 기본 환경 및 폴더 생성해서 적용합니다.

---
+++ QuizLMS/src/main/java/com/Quiz/lms/QuizLmsApplication.java
@@ -0,0 +1,13 @@
+package com.Quiz.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizLmsApplication.class, args);
+	}
+
+}
---
+++ QuizLMS/src/test/java/com/Quiz/lms/QuizLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Quiz.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
