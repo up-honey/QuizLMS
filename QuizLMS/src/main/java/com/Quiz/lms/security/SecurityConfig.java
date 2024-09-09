632b8b6b71ea95f4bfe1a7095363077b96ddb1e5
shshsho <89124179+shshsho@users.noreply.github.com>
2024-09-09 14:39:38 +0900

    오류 수정

--- QuizLMS/src/main/java/com/Quiz/lms/security/SecurityConfig.java
+++ QuizLMS/src/main/java/com/Quiz/lms/security/SecurityConfig.java
@@ -1,26 +1,65 @@
 package com.Quiz.lms.security;
 
+import java.util.Arrays;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletResponse;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	//로그인 유지 관련
+	@Autowired
+    private UserDetailsService userDetailsService;
+	
+	@Autowired
+    private DataSource dataSource;
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+	    AuthenticationManager authenticationManager = authenticationConfiguration.getAuthenticationManager();
+	    if (authenticationManager == null) {
+	        throw new IllegalStateException("AuthenticationManager is not configured properly");
+	    }
+	    return authenticationManager;
+	}
+
 
     @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+	
+	//보안 설정
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http	
             .authenticationProvider(authenticationProvider())
 //            .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
@@ -61,23 +100,42 @@ public class SecurityConfig {
         	)
             .cors(cors -> cors.configurationSource(corsConfigurationSource()));
         
-
         return http.build();
     }
 
-
+    
+    //CORS 관련 리액트 도메인 요청 허용하기
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 리액트 앱 주소
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);  // 데이터베이스 설정
+        return tokenRepository;
+    }
+    
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    /*@Bean
-    public UserDetailsService userDetailsService() {
-        var user = User.withUsername("user")
-                .password("{noop}password") // 암호화 처리 필요
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }*/
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        var user = User.withUsername("user")
+//                .password("{noop}password") // 암호화 처리 필요
+//                .roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
 }
-
